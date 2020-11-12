17dc10afb2a18788aa9f4361fa41d4b804889238
Sarthak Jariwala <jariwala@uw.edu>
2020-11-11 19:41:35 -0800

    still fixing setup.sh.....

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\
-email = \"jariwala@uw.edu\"\
+echo "[general]
+email = \"jariwala@uw.edu\"
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\
-headless = true\
-enableCORS=false\
-port = $PORT\"\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
